05ee20310f8ab536416f8f110552c9a1a9b3e0ff
madhavmuktesh <mukteshmadhava@gmail.com>
2026-06-28 04:30:40 +0530

    Fix API

--- frontend1/src/pages/user/signin.jsx
+++ frontend1/src/pages/user/signin.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../context/AuthContext.jsx';
 import { loginAPI } from '../../services/api.js';
 import '../../styles/signin.css';
 
